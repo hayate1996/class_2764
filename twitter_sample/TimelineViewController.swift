cae1cf79c67f80cc8d212e85adf7d1ddb4c01d6d
hiro.nagami <hiro.nagami@gmail.com>
2015-09-05 10:26:28 +0900

    fixed

--- twitter_sample/TimelineViewController.swift
+++ twitter_sample/TimelineViewController.swift
@@ -7,21 +7,22 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
 class TimelineViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
 
     let cellIdentifier = "tweetCell"
-    var tweets:[Dictionary<String, AnyObject>]?
+    var tweets = []
+    
+    var twAccount = ACAccount()
 
     // タイムライン画面のローディング(ライフサイクルメソッド)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // ツイートデータの初期化
-        tweets = [  ["name": "Hiro", "description": "My name is AAA", "icon":"twitter_icon"],
-                    ["name": "Account BBB", "description": "My name is BBB", "icon":"user_b_icon"],
-                    ["name": "Account CCC", "description": "My name is CCC", "icon":"user_c_icon"],
-                    ["name": "Account BBB", "description": "How old are you, CCC?", "icon":"user_b_icon"],
-                    ["name": "Account CCC", "description": "My old is 17.", "icon":"user_c_icon"]]
+        navigationItem.title = twAccount.username
+        tableView.contentOffset.y = -self.refreshControl!.frame.size.height
+        refresh(refreshControl!)
     }
 
     // メモリーが不足した時にOSから呼ばれる(ライフサイクルメソッド)
@@ -40,11 +41,8 @@ class TimelineViewController: UITableViewController, UITableViewDelegate, UITabl
     // テーブルがsectionごとに持つセルの数を指定(UITableView ライフサイクルメソッド)
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // ツイートarrayの要素の数をTableViewにreturn
-        if let tweetsList = tweets {
-            return tweetsList.count
-        }
 
-        return 0
+        return tweets.count
     }
 
     // 各セルをsection, rowごとに初期化(UITableView ライフサイクルメソッド)
@@ -59,11 +57,17 @@ class TimelineViewController: UITableViewController, UITableViewDelegate, UITabl
         }
 
         // セルに表示するサムネ・ツイート・名前の設定
-        var aCell = cell!
-        aCell.imageView?.image = UIImage(named: (tweets![indexPath.row]["icon"] as? String)!)
-        aCell.textLabel?.text = tweets![indexPath.row]["description"] as? String
-        aCell.detailTextLabel?.text = tweets![indexPath.row]["name"] as? String
-
+        let tweet = tweets[indexPath.row] as! Dictionary<String,AnyObject>
+        let text = tweet["text"] as! String
+        let user = tweet["user"] as! Dictionary<String,AnyObject>
+        let name = user["name"] as! String
+        let image = UIImage(data: NSData(contentsOfURL: NSURL(string: user["profile_image_url"] as! String)!)!)
+        
+        let aCell = cell!
+        aCell.imageView?.image = image
+        aCell.textLabel?.text = text
+        aCell.detailTextLabel?.text = name
+        
         return aCell
     }
 
@@ -75,6 +79,36 @@ class TimelineViewController: UITableViewController, UITableViewDelegate, UITabl
     // セルをタップした時に呼ばれる(UITableView ユーザーアクションメソッド)
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("showTimelineDetailViewController", sender: nil)
+    }
+    
+    //  Twitter APIを使ってタイムラインを取得しtweetsに保存する
+    func fetchTimeline() {
+        let URL = NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")
+        
+        let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, URL: URL, parameters: nil)
+        request.account = twAccount
+        
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        refreshControl?.beginRefreshing()
+        
+        request.performRequestWithHandler { (data, response, error:NSError?) -> Void in
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            self.refreshControl?.endRefreshing()
+            
+            if error != nil {
+                println("Fetching Error: \(error)")
+                return;
+            }
+            
+            self.tweets = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: nil) as! NSArray
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadData()
+            })
+        }
+    }
+    @IBAction func refresh(sender: AnyObject) {
+        fetchTimeline()
     }
     
 }
--- twitter_sample/ViewController.swift
+++ twitter_sample/ViewController.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
 class ViewController: UIViewController {
 
+    var accountStore = ACAccountStore()   // 追加
+    var twAccount = ACAccount()           // 追加
+    var accounts = [ACAccount]()          // 追加
+    
     @IBOutlet var loginButton: UIButton!
     var accountsList:[Dictionary<String,String>]?
 
     // ログイン画面のローディング(ライフサイクルメソッド)
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // アカウントデータを配列に持たせる
-        accountsList = [["name":"@ken"], ["name":"@jeff"], ["name":"@chris"]]
     }
 
     // メモリーが不足した時にOSから呼ばれる(ライフサイクルメソッド)
@@ -26,35 +29,67 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    
+    
     // ログインボタンを押すと呼ばれる(ユーザーアクションメソッド)
     @IBAction func tappedLoginButton(sender: AnyObject) {
-        // 1. アクションシートの初期化
-        // 1-1. アクションシートの作成・タイトルの初期化
-        var accountSelectionSheet = UIAlertController()
-        accountSelectionSheet.title = "Select Account"
-
-        // 1-2. アカウントが選択された時の処理 (クロージャー)
-        let selectAction =  { (var action: UIAlertAction!) -> Void in
-            self.performSegueWithIdentifier("segueTimelineViewController", sender: nil)
-        }
-
-        // 1-3. アカウントのリストと選択時の処理をアクションシートへ追加
-        if let accounts:[Dictionary<String, String>] = accountsList{
-            for account in accounts {
-                // accountListから１つづつアカウントを読み込んで、アクションシートのボタンに追加
-                // その際、それぞれのボタンにselectAction処理を追加している
-                let action = UIAlertAction(title: account["name"]!, style: .Default, handler: selectAction)
-                accountSelectionSheet.addAction(action)
-            }
-        }
-
-        // 1-4. アクションシートにキャンセルボタンの追加・シャンせるアクションの追加
-        let CanceledAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        accountSelectionSheet.addAction(CanceledAction)
-
-        // 2. アクションシートの表示
-        self.presentViewController(accountSelectionSheet, animated: true, completion: nil)
+        getTwitterAccountsFromDevice()
     }
 
+    /* iPhoneに設定したTwitterアカウントの情報を取得する */
+    func getTwitterAccountsFromDevice(){
+        let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+        accountStore.requestAccessToAccountsWithType(accountType, options: nil) { (granted:Bool, aError:NSError?) -> Void in
+            
+            // アカウント取得に失敗したとき
+            if let error = aError {
+                println("Error! - \(error)")
+                return;
+            }
+            
+            // アカウント情報へのアクセス権限がない時
+            if !granted {
+                println("Cannot access to account data")
+                return;
+            }
+            
+            // アカウント情報の取得に成功
+            self.accounts = self.accountStore.accountsWithAccountType(accountType) as! [ACAccount]
+            self.showAndSelectTwitterAccountWithSelectionSheets()
+        }
+    }
+    
+    /* iPhoneに設定したTwitterアカウントの選択画面を表示する */
+    func showAndSelectTwitterAccountWithSelectionSheets() {
+        
+        // アクションシートの設定
+        var alertController = UIAlertController(title: "Select Account", message: "Please select twitter account", preferredStyle: .ActionSheet)
+        
+        for account in accounts {
+            
+            alertController.addAction(UIAlertAction(title: account.username, style: .Default, handler: { (action) -> Void in
+                // 選択したアカウントをtwAccountに保存
+                self.twAccount = account
+                self.performSegueWithIdentifier("segueTimelineViewController", sender: nil)
+            }))
+            
+        }
+        
+        // キャンセルボタンは何もせずにアクションシートを閉じる
+        let CanceledAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(CanceledAction)
+        
+        // アクションシート表示
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // TimelineViewControllerを表示する際に選択したアカウントを渡す
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "segueTimelineViewController" {
+            var vc = segue.destinationViewController as! TimelineViewController
+            vc.twAccount = self.twAccount
+        }
+    }
+    
 }
 
